033db967ccffc156e5210f685f06164f21a802ef
Isaac <icpereiradoprado@gmail.com>
2022-02-25 20:21:09 -0300

    Lista de Exercícios em C#

---
+++ Nivel1/Exercicio1.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace Nivel1
+{
+    class Exercicio1
+    {
+        static void Main1(string[] args)
+        {
+            double estoqueMedio, qtdMinima, qtdMaxima;
+            
+            Console.WriteLine("Digite a quantidade mínima: ");
+            qtdMinima = Convert.ToDouble(Console.ReadLine());
+
+            Console.WriteLine("Digite a quantidade máxima: ");
+            qtdMaxima = Convert.ToDouble(Console.ReadLine());
+
+            estoqueMedio = (qtdMinima + qtdMaxima) / 2;
+
+            Console.WriteLine($"Estoque Médio: {estoqueMedio}");
+
+
+
+        }
+    }
+}
---
+++ Nivel1/Exercicio2.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Nivel1
+{
+    class Exercicio2
+    {
+        static void Main(string[] args) 
+        {
+            double cotacaoDolar, valorDolares, converteReal;
+
+            Console.WriteLine("Digite a cotação dolar: ");
+            cotacaoDolar = Convert.ToDouble(Console.ReadLine());
+
+            Console.WriteLine("Digite a quantide de dolares que você deseja converter: ");
+            valorDolares = Convert.ToDouble(Console.ReadLine());
+
+            converteReal = cotacaoDolar * valorDolares;
+
+            Console.WriteLine("O valor convertido em Reais é: R$ "+String.Format("{0:0.00}", converteReal));
+
+
+        }
+    }
+}
