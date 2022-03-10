4e67d13e36213c730fc82791473b4224090791ec
Isaac <icpereiradoprado@gmail.com>
2022-03-10 20:03:16 -0300

    Lista de exercícios C#

--- Nivel1/Exercicio5.cs
+++ Nivel1/Exercicio5.cs
@@ -8,5 +8,22 @@ namespace Nivel1
 {
     internal class Exercicio5
     {
+        static void Main(string[] args)
+        {
+            double tempoViagem, distanciaPercorrida, litrosUsados;
+            int velocidadeMedia;
+
+            Console.WriteLine("Digite o tempo gasto na viagem: ");
+            tempoViagem = Convert.ToDouble(Console.ReadLine());
+
+            Console.WriteLine("Digite a velocidade média: ");
+            velocidadeMedia = Convert.ToInt32(Console.ReadLine());
+
+            distanciaPercorrida = tempoViagem * Convert.ToDouble(velocidadeMedia);
+
+            litrosUsados = distanciaPercorrida / 12.00;
+
+            Console.WriteLine("A quantidade gasta de combustível é: "+litrosUsados.ToString());
+        }
     }
 }
