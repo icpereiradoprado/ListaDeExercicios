b31efddf5074576bc22fe06d6f0b01cc39800f62
isaac <icpereiradoprado@gmail.com>
2022-03-28 17:01:45 -0300

    Lista de Exercicio Csharp

---
+++ Nivel5/Exercicio45.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Nivel5
+{
+    internal class Exercicio45
+    {
+        static void Main45(string[] args)
+        {
+            int qtdElementos;
+            Console.WriteLine("Digite a quantidade de números que você deseja adicionar no vetor: ");
+            qtdElementos = Convert.ToInt32(Console.ReadLine());
+            
+            int[] numeros = new int[qtdElementos];
+
+            for(int i = 0; i < qtdElementos; i++)
+            {
+                Console.WriteLine($"Digite o {i}º número: ");
+                numeros[i] = Convert.ToInt32(Console.ReadLine());
+            }
+            foreach(var j in numeros)
+            {
+                Console.WriteLine($"Lista original: {j}");
+            }
+
+            Array.Reverse(numeros);
+            foreach (var k in numeros)
+            {
+                Console.WriteLine($"Lista invertida: {k}");
+            }
+            
+        }
+    }
+}
---
+++ Nivel5/Exercicio46.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Nivel5
+{
+    internal class Exercicio46
+    {
+    }
+}
